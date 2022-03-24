fb5ee6bf26dd1d6dfc0c65c702b8f6c9b9b1434e
Idan Regev <idanre1@gmail.com>
2022-03-24 15:18:36 +0200

    Update install_hass.sh

--- hass/install_hass.sh
+++ hass/install_hass.sh
@@ -4,7 +4,7 @@ apt-yes update
 apt-yes upgrade
 apt-yes install python3 python3-dev python3-venv python3-pip libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0-dev tzdata
 
-sudo useradd -rm homeassistant -G dialout,gpio,i2c
+sudo useradd -rm homeassistant -G dialout,i2c
 sudo mkdir /srv/homeassistant
 sudo chown homeassistant:homeassistant /srv/homeassistant
 sudo -u homeassistant -H -s
